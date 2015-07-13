6f6a0824899b3a21cadbd0b3f0512b8425e7085f
Ross Waguespack <rosswaguespack@gmail.com>
2015-07-12 20:13:50 -0500

    Restructured class files and added test

---
+++ movie.rb
@@ -0,0 +1,35 @@
+class Movie
+  attr_reader :rank
+  attr_accessor :title
+
+  def initialize(title, rank=0)
+    @title = title.capitalize
+    @rank = rank
+  end
+  
+  def thumbs_up
+    @rank += 1
+  end
+
+  def thumbs_down
+    @rank -= 1
+  end
+
+  def to_s #listing
+    "#{@title} has a rank of #{@rank}"
+  end
+end
+
+if __FILE__ == $0
+  movie = Movie.new("goonies", 10)
+  puts movie.title
+  puts movie.rank
+
+  movie.thumbs_up
+  movie.thumbs_up
+  puts movie.rank
+
+  movie.thums_down
+  puts movie.rank
+  puts movie
+end
